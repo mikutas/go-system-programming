7d2889f6fad83a3fb7bed3e95735384b903a646c
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-05-29 21:45:38 +0900

    [update]2.9 Q2.1

--- 02_io.Writer/2.4/2.4.7-1.go
+++ 02_io.Writer/2.4/2.4.7-1.go
@@ -7,5 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %v", time.Now())
+	fmt.Fprintf(os.Stdout, "Write with os.Stdout at %v\n", time.Now())
+	fmt.Fprintf(os.Stdout, "%d\n", 1)
+	fmt.Fprintf(os.Stdout, "%s\n", "1.1")
+	fmt.Fprintf(os.Stdout, "%f\n", 1.1)
 }
