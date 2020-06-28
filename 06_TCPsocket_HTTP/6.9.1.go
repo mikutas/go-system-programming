b01e39d50f1d506a768731c154c1708a53f1ecb8
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-06-29 00:24:46 +0900

    [fix]6.9.1
    Keep-Alive版のサーバーを改修、main()は省略と書いているがmain()そのままでいいわけではない

--- 06_TCPsocket_HTTP/6.9.1.go
+++ 06_TCPsocket_HTTP/6.9.1.go
@@ -87,47 +87,6 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// Accept後のソケットで何度も応答を返すためにループ
-			for {
-				// タイムアウトを設定
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-				// リクエストを読み込む
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					// タイムアウトもしくはソケットクローズ時は終了
-					// それ以外はエラーにする
-					neterr, ok := err.(net.Error) // ダウンキャスト
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-
-				// リクエストを表示
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				content := "Hello World\n"
-
-				fmt.Println(string(dump))
-				// レスポンスを書き込む
-				// HTTP/1.1かつ、ContentLengthの設定が必要
-				response := http.Response{
-					StatusCode:    200,
-					ProtoMajor:    1,
-					ProtoMinor:    1,
-					ContentLength: int64(len(content)),
-					Body:          ioutil.NopCloser(strings.NewReader(content)),
-				}
-				response.Write(conn)
-			}
-		}()
+		processSession(conn)
 	}
 }
