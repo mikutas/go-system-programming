a488b6ab89cf622471c1c67632bf41c57a1957fd
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-07-15 19:50:45 +0900

    feat: 10.6.3 ディレクトリ内の一覧を取得できるようにする
    ローカルのディレクトリで試すとエラーが出る
    accessとlistxattrがnot implemented
    いくつかのディレクトリがno such file or directory

--- 10_depths_of_filesystem/10.6.2-4.go
+++ 10_depths_of_filesystem/10.6.2-4.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/billziss-gh/cgofuse/fuse"
 	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/fileblob"
 	_ "gocloud.dev/blob/s3blob"
 )
 
@@ -30,4 +32,55 @@ func main() {
 	cf := &CloudFileSystem{bucket: b}
 	host := fuse.NewFileSystemHost(cf)
 	host.Mount(os.Args[2], os.Args[3:])
+}
+
+func (cf *CloudFileSystem) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
+	if path == "/" {
+		stat.Mode = fuse.S_IFDIR | 0555
+		return 0
+	}
+	ctx := context.Background()
+	name := strings.TrimLeft(path, "/")
+	a, err := cf.bucket.Attributes(ctx, name)
+	if err != nil {
+		_, err := cf.bucket.Attributes(ctx, name+"/")
+		if err != nil {
+			return -fuse.ENOENT
+		}
+		stat.Mode = fuse.S_IFDIR | 0555
+	} else {
+		stat.Mode = fuse.S_IFREG | 0444
+		stat.Size = a.Size
+		stat.Mtim = fuse.NewTimespec(a.ModTime)
+	}
+	stat.Nlink = 1
+	return 0
+}
+
+func (cf *CloudFileSystem) Readdir(path string,
+	fill func(name string, stat *fuse.Stat_t, ofst int64) bool,
+	ofst int64, fh uint64) (errc int) {
+	ctx := context.Background()
+	fill(".", nil, 0)
+	fill("..", nil, 0)
+	prefix := strings.TrimLeft(path, "/")
+	if prefix != "" {
+		prefix = prefix + "/"
+	}
+	i := cf.bucket.List(&blob.ListOptions{
+		Prefix:    prefix,
+		Delimiter: "/",
+	})
+	for {
+		o, err := i.Next(ctx)
+		if err != nil {
+			break
+		}
+		key := o.Key[len(prefix):]
+		if len(key) == 0 {
+			continue
+		}
+		fill(strings.TrimRight(key, "/"), nil, 0)
+	}
+	return 0
 }
