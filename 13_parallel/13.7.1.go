3fc1c5a0510d44f848c6eddcb2212c1640f1f3aa
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-07-23 11:18:47 +0900

    feat: 13.7.1 sync.Mutex / sync.RWMutex

---
+++ 13_parallel/13.7.1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var id int
+
+func generateId(mutex *sync.Mutex) int {
+	// Lock() / Unlock()をペアで呼び出してロックする
+	mutex.Lock()
+	id++
+	result := id
+	mutex.Unlock()
+	return result
+}
+
+func main() {
+	// sync.Mutex構造体の変数宣言
+	// 次の宣言をしてもポインタ型になるだけで正常に動作する
+	// mutex := new(sync.Mutex)
+	var mutex sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			fmt.Printf("id: %d\n", generateId(&mutex))
+		}()
+	}
+}
