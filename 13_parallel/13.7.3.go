94b9c8a015d913170acf704cf8dc044b568d15d4
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2020-07-23 11:33:01 +0900

    feat: 13.7.3 sync.Once

---
+++ 13_parallel/13.7.3.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func initialize() {
+	fmt.Println("初期化処理")
+}
+
+var once sync.Once
+
+func main() {
+	// 3回呼び出しても一度しか呼ばれない
+	once.Do(initialize)
+	once.Do(initialize)
+	once.Do(initialize)
+}
